de2f6e2d79bdbabe2a0f575bb04a30cf4ea4855b
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2026-02-03 00:39:03 +0100

    recent update

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,30 +1,14 @@
+import { Slab } from "react-loading-indicators";
+
 export default function Loading() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-      
-      {/* Loader Wrapper */}
-      <div className="relative flex items-center justify-center">
-
-        {/* Outer Ring */}
-        <div className="
-          w-24 h-24
-          rounded-full
-          border-4 border-[#7BA717]/30
-          animate-spin
-        " />
-
-        {/* Inner Pulse Circle */}
-        <div className="
-          absolute
-          w-12 h-12
-          bg-[#7BA717]
-          rounded-full
-          animate-pulse
-          shadow-lg
-        " />
-
-      </div>
-
+      <Slab
+        color="#7BA717"      // your brand green
+        size="medium"
+        text=""
+        textColor="#7BA717"
+      />
     </div>
   );
 }
